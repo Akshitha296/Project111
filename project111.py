62e0ff6b2adf11df54ef955543686e0b4c994b71
anonymouse <70026624+Akshitha296@users.noreply.github.com>
2021-08-24 12:47:04 -0400

    Add files via upload

---
+++ project111.py
@@ -0,0 +1,61 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("medium_data.csv")
+
+data = df["claps"].tolist()
+
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+
+print("Mean of popultion: ",mean)
+print("Standard deviation of population: ",std_deviation)
+
+mean_list = []
+for j in range(0, 1000):
+    dataset = []
+    for i in range(0, 100):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    mean_list.append(mean)
+
+def show_fig(mean_list):
+    df = mean_list
+    fig = ff.create_distplot([df], ["claps"], show_hist=False)
+    fig.show()
+
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+print("std1",first_std_deviation_start, first_std_deviation_end)
+print("std2",second_std_deviation_start, second_std_deviation_end)
+print("std3",third_std_deviation_start,third_std_deviation_end)
+
+fig = ff.create_distplot([mean_list], ["claps"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 START"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 START"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_start,third_std_deviation_start], y=[0,0.17], mode="lines", name="STANDARD DEVIATION 3 START"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end], y=[0,0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+sample_std_deviation = statistics.stdev(mean_list)
+sample_mean = statistics.mean(mean_list)
+print("Mean of sampling distribution: ",sample_mean)
+print("Standard deviation of sampling distribution: ", sample_std_deviation)
+
+fig = ff.create_distplot([mean_list], ["claps"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.20], mode="lines", name="MEAN"))
+fig.show()
+
+z_score = (mean - sample_mean)/std_deviation
+print("The z score is = ",z_score)
